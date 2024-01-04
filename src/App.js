fb10ede9f86f6f38092e2c75c44114592fe75d8e
shivv004 <shiv.singh1104@gmail.com>
2024-01-05 00:19:16 +0530

    v0.1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Contact from './components/Contact';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header/>}>
+          <Route index element={<Main />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import './css/Main.css';
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>
+      <img id="img" src="moma.png" alt="moma" style={{width:'100%'}}></img>
+    </div>
+  )
+}
