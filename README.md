dec5c524582ed946ba6854cb50235f7842c5066d
shivv004 <shiv.singh1104@gmail.com>
2024-02-09 15:58:53 +0530

    First commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Sedbuzz
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
+import MostRecent from './components/MostRecent';
+import Trending from './components/Trending';
+import Technology from './components/Technology';
+import Gaming from './components/Gaming';
+import Science from './components/Science';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Search from './components/Search';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+export default function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Main />} />
+          <Route path="/category/most-recent" element={<MostRecent />} />
+          <Route path="/category/trending" element={<Trending />} />
+          <Route path="/category/technology" element={<Technology />} />
+          <Route path="/category/gaming" element={<Gaming />} />
+          <Route path="/category/science" element={<Science />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<Signin/>} />
+          <Route path="/signup" element={<Signup/>} />
+        </Route>
+      </Routes>
+      <Footer/>
+      <ScrollToTop />
+    </BrowserRouter>
+    </>
+  )
+}
