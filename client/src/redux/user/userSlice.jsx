1a3ccc886c91e234fdd1e28d1f6e4c604031d9e3
shivv004 <shiv.singh1104@gmail.com>
2024-02-14 16:07:08 +0530

    signout and delete user functionallity

--- client/src/redux/user/userSlice.jsx
+++ client/src/redux/user/userSlice.jsx
@@ -23,9 +23,41 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
-    },
+        updateStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        },
+    }
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure ,signoutSuccess } = userSlice.actions;
 
 export default userSlice.reducer;
